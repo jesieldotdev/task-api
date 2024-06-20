e1c55ad0358cfe953b6b721ef6857d9ebf79f46d
jesieldotdev <jesiel364@gmail.com>
2024-06-19 22:16:33 -0400

    s

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,12 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './tasks/entities/tasks.entity';
 
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_USERNAME:', process.env.DB_USERNAME);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_DATABASE:', process.env.DB_DATABASE);
+
 @Module({
   imports: [
     ConfigModule.forRoot({
